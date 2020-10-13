1f6e8763bfe77d29e25bee7b02f365393be138da
Andres Alvarez <ndralvz@gmail.com>
2020-10-13 14:29:33 -0300

    minor fixes on utils

--- src/utils/index.js
+++ src/utils/index.js
@@ -2,14 +2,7 @@
 
 const mayus = x => x.slice(0, 1).toUpperCase() + x.slice(1)
 
-const findType = element =>
-	element === "email"
-		? "email"
-		: element === "phone"
-		? "tel"
-		: "text"
-
 const createKey = (value, tag) =>
 	`${value.replace(" ", "").toLowerCase()}-${tag}`
 
-export { mayus, findType, createKey }
+export { mayus, createKey }
